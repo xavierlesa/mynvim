e0f7e0100e6e40f15b73906f40df745019d77ed4
Xavier <xavierlesa@gmail.com>
2026-03-27 21:49:36 -0300

    update

--- lua/plugins/02-lsp.lua
+++ lua/plugins/02-lsp.lua
@@ -72,39 +72,6 @@ return {
 					-- WARN: This is not Goto Definition, this is Goto Declaration.
 					--  For example, in C this would take you to the header.
 					map("gd", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-
-					-- The following two autocommands are used to highlight references of the
-					-- word under your cursor when your cursor rests there for a little while.
-					--    See `:help CursorHold` for information about when this is executed
-					--
-					-- When you move your cursor, the highlights will be cleared (the second autocommand).
-					local client = vim.lsp.get_client_by_id(event.data.client_id)
-					if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-						local highlight_augroup =
-							vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
-						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-							buffer = event.buf,
-							group = highlight_augroup,
-							callback = vim.lsp.buf.document_highlight,
-						})
-
-						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-							buffer = event.buf,
-							group = highlight_augroup,
-							callback = vim.lsp.buf.clear_references,
-						})
-
-						vim.api.nvim_create_autocmd("LspDetach", {
-							group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
-							callback = function(event2)
-								vim.lsp.buf.clear_references()
-								vim.api.nvim_clear_autocmds({
-									group = "kickstart-lsp-highlight",
-									buffer = event2.buf,
-								})
-							end,
-						})
-					end
 				end,
 			})
 
@@ -151,7 +118,17 @@ return {
 				ts_ls = {},
 				vue_ls = {},
 				-- volar = {},
-				pyright = {},
+				pyright = {
+					settings = {
+						python = {
+							analysis = {
+								typeCheckingMode = "basic",
+								autoSearchPaths = true,
+								useLibraryCodeForTypes = true,
+							}
+						}
+					}
+				},
 				ruff = {
 					init_options = {
 					 settings = {
@@ -203,11 +180,12 @@ return {
 
 			-- Force djlsp
 			vim.lsp.config('djlsp', {
-				cmd = { "/Users/xavierlesa/.local/pipx/venvs/django-template-lsp/bin/djlsp", "--enable-log" },
+				cmd = { "/Users/xavierlesa/.local/pipx/venvs/django-template-lsp/bin/djlsp" },
 				filetypes = { "html", "htmldjango" },
 				root_markers = { "manage.py", ".git" },
 				capabilities = vim.tbl_deep_extend("force", {}, capabilities, {}),
 			})
+			vim.lsp.enable('djlsp')
 
 			local null_ls = require("null-ls")
 			null_ls.setup({
